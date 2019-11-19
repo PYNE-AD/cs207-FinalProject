446bd862d47fad1dfc24064978eca64884ca5109
emma <31635156+hua-1998@users.noreply.github.com>
2019-11-19 16:29:27 -0500

    Fixed typos, issues in AutoDiff.py
    I found some typos across the operations I wrote tests for (add, sub, mul, truediv, pow), so I would recommend using this version of AutoDiff.py, or at least these versions of the specific overloaded operations in the final version of AutoDiff.py.

--- docs/AutoDiff_emma.py
+++ docs/AutoDiff_emma.py
@@ -1,4 +1,4 @@
-# Emma's edits to AutoDiff
+# Emma's updated version (fixed overloading)
 
 import numpy as np
 
@@ -33,9 +33,9 @@ class AutoDiff():
 		'''
 		# Convert int or float to array
 		self.val = self._convertNonArray(eval_value)
-		self.der = None if der_value == None else self._convertNonArray(der_value)*self.val**0.0
+		self.der = self._convertNonArray(der_value)*(self.val**0.0)
 		self.n = n
-		self.jacobian = self._calcJacobian(k, n) if jacobian_value == None else jacobian_value*self.val**0.0
+		self.jacobian = self._calcJacobian(k, n, jacobian_value)
 
 	def _convertNonArray(self, value):
 		try:
@@ -44,12 +44,16 @@ class AutoDiff():
 		except:
 			return np.array([[value]])
 
-	def _calcJacobian(self, k, n):
-		if k != 0:
-			rows = self.val.shape[0]
-			seed = np.zeros([rows, n])
-			seed[:, k-1] = 1
-			return seed
+	def _calcJacobian(self, k, n, jacobian_value):
+		if np.all(np.equal(jacobian_value, None)):
+			if k != 0:
+				rows = self.val.shape[0]
+				seed = np.zeros([rows, n])
+				seed[:, k-1] = 1
+				return seed
+		else:
+			return jacobian_value*self.val**0.0
+
 
 	def __add__(self, other):
 		try:
@@ -74,7 +78,7 @@ class AutoDiff():
 			# If trying to add a constant to AutoDiff, only add values. Constant has derivative of 0 so no subtraction needed.
 			return AutoDiff(self.val - other, self.der, self.n, 0, self.jacobian)
 
-	# Account for reverse addition
+	# Account for reverse subtraction
 	def __rsub__(self, other):
 		try:
 			return AutoDiff(other.val - self.val, other.der - self.der, self.n, 0, other.jacobian - self.jacobian)
@@ -99,7 +103,7 @@ class AutoDiff():
 	def __truediv__(self, other):
 		try:
 			# Use quotient rule
-			return AutoDiff(self.val / other.val, self.n, 0, (self.jacobian * other.val - self.val * other.jacobian)/(other.val**2))
+			return AutoDiff(self.val / other.val, (self.der * other.val - self.val * other.der)/(other.val**2), self.n, 0, (self.jacobian * other.val - self.val * other.jacobian)/(other.val**2))
 		except AttributeError:
 			return AutoDiff(self.val / other, self.der / other, self.n, 0, self.jacobian / other)
 
@@ -113,17 +117,17 @@ class AutoDiff():
 
 	def __pow__(self, other):
 		# Convert to float so that negative integers will work
-		other = float(other) if other < 0 else other
+		other = float(other) if type(other)==int else other
 		try:
-			return AutoDiff(self.val**other.val, other.val * (self.val ** (other.val-1)) * self.der + (self.val**other.val) *np.log(np.abs(self.val)) * other.der, self.n, 0, other.val**self.val * (np.log(x) + 1))
+			return AutoDiff(self.val**other.val, other.val * (self.val ** (other.val-1)) * self.der + (self.val**other.val) *np.log(np.abs(self.val)) * other.der, self.n, 0, other.val * (self.val**(other.val-1)) * self.jacobian + (self.val**other.val * np.log(np.abs(self.val)) * other.jacobian))
 		except AttributeError:
 			return AutoDiff(self.val**other, other * (self.der) * self.val**(other-1), self.n, 0, other * (self.jacobian) * self.val**(other-1))
 
 	def __rpow__(self, other):
 		try:
-			return AutoDiff(other.val**self.val, other.val * (self.val ** (other.val-1)) * self.der + (self.val**other.val) *np.log(np.abs(self.val)) * other.der)
+			return AutoDiff(other.val**self.val, other.val * (self.val ** (other.val-1)) * self.der + (self.val**other.val) *np.log(np.abs(self.val)) * other.der, self.n, 0, other.val * (self.val**(other.val-1)) * self.jacobian + (self.val**other.val * np.log(np.abs(self.val)) * other.jacobian))
 		except AttributeError:
-			return AutoDiff(other**self.val, np.log(other) * other**self.val * self.der, self.n, 0, np.log(other) + other**self.val * self.jacobian)
+			return AutoDiff(other**self.val, np.log(other) * other**self.val * self.der, self.n, 0, np.log(other) * other**self.val * self.jacobian)
 
 	# Unary operations
 	# Unary addition: identity
@@ -141,8 +145,8 @@ class AutoDiff():
 
 	def __abs__(self):
 		try:
-			return AutoDiff(abs(self.val), ((self.val * self.der) / abs(self.val) if abs(self.val) != 0 else None),
-				self.n, 0, ((self.val * self.jacobian) / abs(self.val) if abs(self.val) != 0 else None))
+			return AutoDiff(abs(self.val), ((self.val * self.der) / abs(self.val)),
+				self.n, 0, ((self.val * self.jacobian) / abs(self.val)))
 		except AttributeError:
 			return abs(self)
 
