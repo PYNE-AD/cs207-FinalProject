29c7e303ec7ca4e0e0f5cbf18ef28172b706402e
nikhilvdk <nikhil.vanderklaauw@gmail.com>
2019-11-19 18:07:25 -0500

    setup.py edits and packaging

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/PYNE-AD/cs207-FinalProject",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'pytest'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
