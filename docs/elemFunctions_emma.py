2fa64bacd29e8d175a3ab2540941d04ca1a4d752
emma <31635156+hua-1998@users.noreply.github.com>
2019-11-18 20:41:06 -0500

    Updated elemFunctions_emma.py
    Needed to change tan and sqrt functions to deal with domain issues

--- docs/elemFunctions_emma.py
+++ docs/elemFunctions_emma.py
@@ -20,8 +20,8 @@ def sin(x):
 	>>> x = AutoDiff(0.5, 2.0, 1.0)
 	>>> myAutoDiff = sin(x)
 	>>> myAutoDiff.val
-    0.479425538604
-    >>> myAutoDiff.der
+	0.479425538604
+	>>> myAutoDiff.der
 	1.75516512378
 	>>> myAutoDiff.jacobian
 	0.87758256189
@@ -37,7 +37,7 @@ def sin(x):
 
 # Cosine function
 def cos(x):
-    ''' Compute the cosine of an AutoDiff object and its derivative.
+	''' Compute the cosine of an AutoDiff object and its derivative.
 
 	INPUTS
 	======
@@ -59,17 +59,17 @@ def cos(x):
 	-0.479425538604
 
 	'''
-    try:
-        new_val = np.cos(x.val)
-        new_der = -1.0 * np.sin(x.val) * x.der
-        new_jacobian = -1.0 * np.sin(x.val) * x.jacobian
-        return AutoDiff(new_val, new_der, x.n, 0, new_jacobian)
-    except AttributeError:
-        return np.cos(x)
+	try:
+		new_val = np.cos(x.val)
+		new_der = -1.0 * np.sin(x.val) * x.der
+		new_jacobian = -1.0 * np.sin(x.val) * x.jacobian
+		return AutoDiff(new_val, new_der, x.n, 0, new_jacobian)
+	except AttributeError:
+		return np.cos(x)
 
 # Tangent function
 def tan(x):
-    ''' Compute the tangent of an AutoDiff object and its derivative.
+	''' Compute the tangent of an AutoDiff object and its derivative.
 
 	INPUTS
 	======
@@ -91,13 +91,70 @@ def tan(x):
 	1.29844641041
 
 	'''
-    try:
-        # Value undefined when cos(val) = 0
-        new_val = np.tan(x.val) if np.cos(x.val)!=0 else None
-        # Derivative undefined when cos(val) = 0
-        new_der = x.der / (np.cos(x.val)**2.0) if np.cos(x.val)!=0 else None
-        new_jacobian = x.jacobian / (np.cos(x.val)**2.0) if np.cos(x.val)!=0 else None
-        return AutoDiff(new_val, new_der, x.n, 0, new_jacobian)
-    except AttributeError:
-        return np.tan(x)
-        
+	try:
+		# Value and derivative undefined when divisible by pi/2 but not pi
+		if x.val%(np.pi/2)==0 and x.val%np.pi!=0:
+			new_val = None
+			new_der = None
+			new_jacobian = None
+			raise TypeError('Undefined at value')
+		else:
+			new_val = np.tan(x.val)
+			new_der = x.der / (np.cos(x.val)**2.0)
+			new_jacobian = x.jacobian / (np.cos(x.val)**2.0)
+		return AutoDiff(new_val, new_der, x.n, 0, new_jacobian)
+	except AttributeError:
+		if x%(np.pi/2)==0 and x%np.pi!=0:
+			raise TypeError('Undefined at value')
+		else:
+			return np.tan(x)
+	except TypeError:
+		raise
+
+
+# Paulina's square root function
+def sqrt(x):
+	''' Compute the square root an AutoDiff object and its derivative.
+
+	INPUTS
+	======
+	x: an AutoDiff object
+
+	RETURNS
+	=======
+	A new AutoDiff object with calculated value and derivative.
+
+	EXAMPLES
+	========
+	>>> x = AutoDiff(np.array([[5]]).T, np.array([[1]]), 1, 1)
+	>>> myAutoDiff = sqrt(x)
+	>>> myAutoDiff.val
+	2.2360679775
+	>>> myAutoDiff.der
+	0.2236068
+
+	'''
+	try:
+		# Value not defined when x < 0, derivative not defined when x <= 0
+		if x.val < 0.0:
+			new_val = None
+			new_der = None
+			new_jacobian = None
+			raise TypeError('Undefined at value')
+		elif x.val == 0.0:
+			new_val = np.sqrt(x.val)
+			new_der = None
+			new_jacobian = None
+			raise TypeError('Derivative undefined at value')
+		else:
+			new_val = np.sqrt(x.val)
+			new_der = 0.5 * x.val ** (-0.5) * x.der
+			new_jacobian = 0.5 * x.val ** (-0.5) * x.jacobian
+		return AutoDiff(new_val, new_der, x.n, 0, new_jacobian)
+	except AttributeError:
+		if x < 0.0:
+			raise TypeError('Undefined at value')
+		else:
+			return np.sqrt(x)
+	except TypeError:
+		raise
