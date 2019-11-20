dfb75c7da7fe9d1aa2884fbf251275662cf4500b
nikhilvdk <nikhil.vanderklaauw@gmail.com>
2019-11-19 21:04:12 -0500

    fixed hardcoded computations

--- ADPYNE/tests/elemFunctions_test.py
+++ ADPYNE/tests/elemFunctions_test.py
@@ -14,29 +14,29 @@ def test_sin_ad_results():
 	# Positive real numbers
 	x = AutoDiff(0.5, 2.0)
 	f = ef.sin(x)
-	assert f.val == np.array([[0.479425538604203]])
+	assert f.val == np.array([[np.sin(0.5)]])
 	assert f.der == np.array([[np.cos(0.5)*2.0]])
 	assert f.jacobian == np.array([[np.cos(0.5)]])
 	# Negative real numbers
 	y = AutoDiff(-0.5, 2.0)
 	f = ef.sin(y)
-	assert f.val == np.array([[-0.479425538604203]])
+	assert f.val == np.array([[np.sin(-0.5)]])
 	assert f.der == np.array([[np.cos(-0.5)*2.0]])
 	assert f.jacobian == np.array([[np.cos(-0.5)]])
 	# Zero
 	z = AutoDiff(0.0, 2.0)
 	f = ef.sin(z)
-	assert f.val == np.array([[0.0]])
+	assert f.val == np.array([[np.sin(0)]])
 	assert f.der == np.array([[2.0]])
 	assert f.jacobian == np.array([[1.0]])
 
 def test_sin_constant_results():
 	a = ef.sin(5)
-	assert a == -0.9589242746631385
+	assert a == np.sin(5)
 	b = ef.sin(-5)
-	assert b == 0.9589242746631385
+	assert b == np.sin(-5)
 	c = ef.sin(0)
-	assert c == 0.0
+	assert c == np.sin(0)
 
 def test_sin_types():
 	with pytest.raises(TypeError):
@@ -51,29 +51,29 @@ def test_cos_ad_results():
 	# Positive real numbers
 	x = AutoDiff(0.5, 2.0)
 	f = ef.cos(x)
-	assert f.val == np.array([[0.8775825618903728]])
+	assert f.val == np.array([[np.cos(0.5)]])
 	assert f.der == np.array([[np.sin(0.5)*-2.0]])
 	assert f.jacobian == np.array([[np.sin(0.5)*-1.0]])
 	# Negative real numbers
 	y = AutoDiff(-0.5, 2.0)
 	f = ef.cos(y)
-	assert f.val == np.array([[0.8775825618903728]])
+	assert f.val == np.array([[np.cos(-0.5)]])
 	assert f.der == np.array([[np.sin(-0.5)*-2.0]])
 	assert f.jacobian == np.array([[np.sin(-0.5)*-1.0]])
 	# Zero
 	z = AutoDiff(0.0, 2.0)
 	f = ef.cos(z)
-	assert f.val == np.array([[1.0]])
+	assert f.val == np.array([[np.cos(0)]])
 	assert f.der == np.array([[0.0]])
 	assert f.jacobian == np.array([[0.0]])
 
 def test_cos_constant_results():
 	a = ef.cos(5)
-	assert a == 0.2836621854632263
+	assert a == np.cos(5)
 	b = ef.cos(-5)
-	assert b == 0.2836621854632263
+	assert b == np.cos(-5)
 	c = ef.cos(0)
-	assert c == 1.0
+	assert c == np.cos(0)
 
 def test_cos_types():
 	with pytest.raises(TypeError):
@@ -89,19 +89,19 @@ def test_tan_ad_results():
 	# Positive reals
 	x = AutoDiff(0.5, 2.0)
 	f = ef.tan(x)
-	assert f.val == np.array([[0.5463024898437905]])
+	assert f.val == np.array([[np.tan(0.5)]])
 	assert f.der == np.array([[2.0 / (np.cos(0.5)**2)]])
 	assert f.jacobian == np.array([[1.0 / (np.cos(0.5)**2)]])
 	# Negative reals
 	y = AutoDiff(-0.5, 2.0)
 	f = ef.tan(y)
-	assert f.val == np.array([[-0.5463024898437905]])
+	assert f.val == np.array([[np.tan(-0.5)]])
 	assert f.der == np.array([[2.0 / (np.cos(-0.5)**2)]])
 	assert f.jacobian == np.array([[1.0 / (np.cos(-0.5)**2)]])
 	# Zero
 	z = AutoDiff(0.0, 2.0)
 	f = ef.tan(z)
-	assert f.val == np.array([[0.0]])
+	assert f.val == np.array([[np.tan(0)]])
 	assert f.der == np.array([[2.0]])
 	assert f.jacobian == np.array([[1.0]])
 
@@ -115,11 +115,11 @@ def test_tan_ad_results():
 
 def test_tan_constant_results():
 	a = ef.tan(5)
-	assert a == -3.380515006246585
+	assert a == np.tan(5)
 	b = ef.tan(-5)
-	assert b == 3.380515006246585
+	assert b == np.tan(-5)
 	c = ef.tan(0)
-	assert c == 0.0
+	assert c == np.tan(0)
 	# Value undefined when cos(val)==0
 	with pytest.warns(RuntimeWarning):
 		d = ef.tan(np.pi/2)
@@ -138,7 +138,7 @@ def test_arcsin_ad_results():
 	# positive real numbers
 	x = AutoDiff(0.5, 2)
 	f = ef.arcsin(x)
-	assert f.val == np.array([[0.5235987755982988]])
+	assert f.val == np.array([[np.arcsin(0.5)]])
 	assert f.der == np.array([[2/np.sqrt(1-0.5**2)]])
 	assert f.jacobian == np.array([[1/np.sqrt(1-0.5**2)]])
 
@@ -154,7 +154,7 @@ def test_arcsin_ad_results():
 	with pytest.warns(RuntimeWarning):
 		y = AutoDiff(1, 2)
 		f = ef.arcsin(y)
-		assert f.val == np.array([[1.5707963267948966]])
+		assert f.val == np.array([[np.arcsin(1)]])
 		assert np.isinf(f.der[0][0])
 		assert np.isinf(f.jacobian[0][0])
 
@@ -167,10 +167,10 @@ def test_arcsin_ad_results():
 
 def test_arcsin_constant_results():
 	a = ef.arcsin(0.7)
-	assert a == 0.775397496610753
+	assert a == np.arcsin(0.7)
 
 	b = ef.arcsin(0)
-	assert b == 0.0
+	assert b == np.arcsin(0)
 
 	with pytest.warns(RuntimeWarning):
 		c = ef.arcsin(-5)
@@ -189,7 +189,7 @@ def test_arccos_ad_results():
 	# positive real numbers
 	x = AutoDiff(0.5, 2)
 	f = ef.arccos(x)
-	assert f.val == np.array([[1.0471975511965976]])
+	assert f.val == np.array([[np.arccos(0.5)]])
 	assert f.der == np.array([[-2/np.sqrt(1-0.5**2)]])
 	assert f.jacobian == np.array([[-1/np.sqrt(1-0.5**2)]])
 
@@ -205,23 +205,23 @@ def test_arccos_ad_results():
 	with pytest.warns(RuntimeWarning):
 		y = AutoDiff(1, 2)
 		f = ef.arccos(y)
-		assert f.val == np.array([[0]])
+		assert f.val == np.array([[np.arccos(1)]])
 		assert np.isneginf(f.der[0][0])
 		assert np.isneginf(f.jacobian[0][0])
 
 	# zero
 	z = AutoDiff(0, 2)
 	f = ef.arccos(z)
-	assert f.val == np.array([[1.5707963267948966]])
-	assert f.der == np.array([[-2.0]])
-	assert f.jacobian == np.array([[-1.0]])
+	assert f.val == np.array([[np.arccos(0)]])
+	assert f.der == np.array([[-2/np.sqrt(1-0**2)]])
+	assert f.jacobian == np.array([[-1/np.sqrt(1-0**2)]])
 
 def test_arccos_constant_results():
 	a = ef.arccos(0.7)
-	assert a == 0.7953988301841436
+	assert a == np.arccos(0.7)
 
 	b = ef.arccos(0)
-	assert b == 1.5707963267948966
+	assert b == np.arccos(0)
 
 	with pytest.warns(RuntimeWarning):
 		c = ef.arccos(-5)
@@ -241,30 +241,30 @@ def test_arctan_ad_results():
 	# positive real numbers
 	x = AutoDiff(3, 2)
 	f = ef.arctan(x)
-	assert f.val == np.array([[1.2490457723982544]])
+	assert f.val == np.array([[np.arctan(3)]])
 	assert f.der == np.array([[2/(1+(3)**2)]])
 	assert f.jacobian == np.array([[1/(1+(3)**2)]])
 	# negative real numbers
 	y = AutoDiff(-2, 2)
 	f = ef.arctan(y)
-	assert f.val == np.array([[-1.1071487177940906]])
+	assert f.val == np.array([[np.arctan(-2)]])
 	assert f.der == np.array([[2/(1+(-2)**2)]])
 	assert f.jacobian == np.array([[1/(1+(-2)**2)]])
 	# zero
 	z = AutoDiff(0, 2)
 	f = ef.arctan(z)
-	assert f.val == np.array([[0.0]])
-	assert f.der == np.array([[2.0]])
-	assert f.jacobian == np.array([[1.0]])
+	assert f.val == np.array([[np.arctan(0)]])
+	assert f.der == np.array([[2/(1+(0)**2)]])
+	assert f.jacobian == np.array([[1/(1+(0)**2)]])
 
 
 def test_arctan_constant_results():
 	a = ef.arctan(0.7)
-	assert a == 0.6107259643892086
+	assert a == np.arctan(0.7)
 	b = ef.arctan(-5)
-	assert b == -1.373400766945016
+	assert b == np.arctan(-5)
 	c = ef.arctan(0)
-	assert c == 0.0
+	assert c == np.arctan(0)
 
 def test_arctan_types():
 	with pytest.raises(TypeError):
@@ -278,13 +278,13 @@ def test_arctan_types():
 def test_sinh_results():
 	X = AutoDiff(0.5, 2)
 	f = ef.sinh(X)
-	assert f.val == 0.5210953054937474
-	assert f.der == 2.2552519304127614
-	assert f.jacobian == 1.1276259652063807
+	assert f.val == np.sinh(0.5)
+	assert f.der == np.array([[2*np.cosh(0.5)]])
+	assert f.jacobian == np.array([[1*np.cosh(0.5)]])
 
 def test_sinh_constant_results():
 	a = ef.sinh(0.5)
-	assert a == 0.5210953054937474
+	assert a == np.sinh(0.5)
 
 def test_sinh_types():
 	with pytest.raises(TypeError):
@@ -296,13 +296,13 @@ def test_sinh_types():
 def test_cosh_results():
 	X = AutoDiff(0.5, 2)
 	f = ef.cosh(X)
-	assert f.val == 1.1276259652063807
-	assert f.der == 1.0421906109874948
-	assert f.jacobian == 0.5210953054937474
+	assert f.val == np.cosh(0.5)
+	assert f.der == np.array([[np.sinh(0.5)*2]])
+	assert f.jacobian == np.array([[np.sinh(0.5)*1]])
 
 def test_cosh_constant_results():
 	a = ef.cosh(0.5)
-	assert a == 1.1276259652063807
+	assert a == np.cosh(0.5)
 
 def test_cosh_types():
 	with pytest.raises(TypeError):
@@ -314,13 +314,13 @@ def test_cosh_types():
 def test_tanh_results():
 	X = AutoDiff(0.5, 2)
 	f = ef.tanh(X)
-	assert f.val == 0.46211715726000974
-	assert f.der == 1.572895465931855
-	assert f.jacobian == 0.7864477329659275
+	assert f.val == np.tanh(0.5)
+	assert f.der == np.array([[2/(np.cosh(0.5)**2)]])
+	assert f.jacobian == np.array([[1/(np.cosh(0.5)**2)]])
 
 def test_tanh_constant_results():
 	a = ef.tanh(0.5)
-	assert a == 0.46211715726000974
+	assert a == np.tanh(0.5)
 
 def test_tanh_types():
 	with pytest.raises(TypeError):
@@ -335,29 +335,29 @@ def test_arcsinh_ad_results():
 	# positive real numbers
 	x = AutoDiff(1, 2)
 	f = ef.arcsinh(x)
-	assert f.val == 0.881373587019543
-	assert f.der == 2/np.sqrt(2)
-	assert f.jacobian == 1/np.sqrt(2)
+	assert f.val == np.arcsinh(1)
+	assert f.der == np.array([[((2)/np.sqrt((1)**2 + 1))]])
+	assert f.jacobian == np.array([[((1)/np.sqrt((1)**2 + 1))]])
 	# negative real numbers
 	y = AutoDiff(-1, 2)
 	f = ef.arcsinh(y)
-	assert f.val == -0.881373587019543
-	assert f.der == 2/np.sqrt(2)
-	assert f.jacobian == 1/np.sqrt(2)
+	assert f.val == np.arcsinh(-1)
+	assert f.der == np.array([[((2)/np.sqrt((-1)**2 + 1))]])
+	assert f.jacobian == np.array([[((1)/np.sqrt((-1)**2 + 1))]])
 	# zero
 	z = AutoDiff(0, 2)
 	f = ef.arcsinh(z)
-	assert f.val == 0.0
-	assert f.der == 2.0
-	assert f.jacobian == 1.0
+	assert f.val == np.arcsinh(0)
+	assert f.der == np.array([[((2)/np.sqrt((0)**2 + 1))]])
+	assert f.jacobian == np.array([[((1)/np.sqrt((0)**2 + 1))]])
 
 def test_arcsinh_constant_results():
 	a = ef.arcsinh(5)
-	assert a == 2.3124383412727525
+	assert a == np.arcsinh(5)
 	b = ef.arcsinh(-5)
-	assert b == -2.3124383412727525
+	assert b == np.arcsinh(-5)
 	c = ef.arcsinh(0)
-	assert c == 0.0
+	assert c == np.arcsinh(0)
 
 def test_arcsinh_types():
 	with pytest.raises(TypeError):
@@ -372,9 +372,9 @@ def test_arccosh_ad_results():
 	# derivative defined at positive real numbers x > 1
 	x = AutoDiff(1.1, 2)
 	f = ef.arccosh(x)
-	assert f.val == 0.4435682543851153
-	assert f.der == 2/np.sqrt(1.1**2 - 1)
-	assert f.jacobian == 1/np.sqrt(1.1**2 - 1)
+	assert f.val == np.arccosh(1.1)
+	assert f.der == np.array([[((2)/np.sqrt((1.1)**2 - 1))]])
+	assert f.jacobian == np.array([[((1)/np.sqrt((1.1)**2 - 1))]])
 	# value defined at x = 1, derivative not defined
 	with pytest.warns(RuntimeWarning):
 		y = AutoDiff(1, 2)
@@ -391,7 +391,7 @@ def test_arccosh_ad_results():
 
 def test_arccosh_constant_results():
 	a = ef.arccosh(5)
-	assert a == 2.2924316695611777
+	assert a == np.arccosh(5)
 	# value not defined at x < 1
 	with pytest.warns(RuntimeWarning):
 		a = ef.arccosh(0.9)
@@ -409,14 +409,15 @@ def test_arctanh_ad_results():
 	# value defined at real numbers (-1, 1)
 	x = AutoDiff(0.5, 2)
 	f = ef.arctanh(x)
-	assert f.val == 0.5493061443340549
-	assert f.der == 2/(1-(0.5)**2)
-	assert f.jacobian == 1/(1-(0.5)**2)
+	assert f.val == np.arctanh(0.5)
+	assert f.der == np.array([[((2)/(1-(0.5)**2))]])
+	assert f.jacobian == np.array([[((1)/(1-(0.5)**2))]])
+	
 	y = AutoDiff(-0.99999, 2)
 	f = ef.arctanh(y)
-	assert f.val == -6.1030338227611125
-	assert f.der == 2/(1-(-0.99999)**2)
-	assert f.jacobian == 1/(1-(-0.99999)**2)
+	assert f.val == np.arctanh(-0.99999)
+	assert f.der == np.array([[((2)/(1-(-0.99999)**2))]])
+	assert f.jacobian == np.array([[((1)/(1-(-0.99999)**2))]])
 	# test for real numbers not in (-1, 1)
 	with pytest.warns(RuntimeWarning):
 		z = AutoDiff(-1, 2)
@@ -428,14 +429,14 @@ def test_arctanh_ad_results():
 		z = AutoDiff(10, 2)
 		f = ef.arctanh(z)
 		assert np.isnan(f.val)
-		assert f.der == ((2)/(1-10**2))
-		assert f.jacobian == ((1)/(1-10**2))
+		assert f.der == np.array([[((2)/(1-(10)**2))]])
+		assert f.jacobian == np.array([[((1)/(1-(10)**2))]])
 
 def test_arctanh_constant_results():
 	a = ef.arctanh(0.99999)
-	assert a == 6.1030338227611125
+	assert a == np.arctanh(0.99999)
 	b = ef.arctanh(-0.001)
-	assert b == -0.0010000003333335333
+	assert b == np.arctanh(-0.001)
 	with pytest.warns(RuntimeWarning):
 		ef.arctanh(1)
 	with pytest.warns(RuntimeWarning):
@@ -454,27 +455,27 @@ def test_exp_ad_results():
 	# positive numbers
 	x = AutoDiff(10, 2)
 	f = ef.exp(x)
-	assert f.val == 22026.465794806718
-	assert f.der == 2*22026.465794806718
-	assert f.jacobian == 22026.465794806718
+	assert f.val == np.exp(10)
+	assert f.der == 2*np.exp(10)
+	assert f.jacobian == 1*np.exp(10)
 	y = AutoDiff(-5, 2)
 	f = ef.exp(y)
-	assert f.val == 0.006737946999085467
-	assert f.der == 2*0.006737946999085467
-	assert f.jacobian == 0.006737946999085467
+	assert f.val == np.exp(-5)
+	assert f.der == 2*np.exp(-5)
+	assert f.jacobian == 1*np.exp(-5)
 	z = AutoDiff(0, 2)
 	f = ef.exp(z)
-	assert f.val == 1.0
-	assert f.der == 2.0
-	assert f.jacobian == 1.0
+	assert f.val == np.exp(0)
+	assert f.der == 2*np.exp(0)
+	assert f.jacobian == 1*np.exp(0)
 
 def test_exp_constant_results():
 	a = ef.exp(0)
-	assert a == 1
+	assert a == np.exp(0)
 	b = ef.exp(5)
-	assert b == 148.4131591025766
+	assert b == np.exp(5)
 	c = ef.exp(-10)
-	assert c == 4.5399929762484854e-05
+	assert c == np.exp(-10)
 
 def test_exp_types():
 	with pytest.raises(TypeError):
@@ -488,9 +489,9 @@ def test_log_results():
 	# derivative defined at real numbers x ≠ 0
 	X = AutoDiff(0.5, 2)
 	f = ef.log(X)
-	assert f.val == -0.6931471805599453
-	assert f.der == 4
-	assert f.jacobian == 2
+	assert f.val == np.log(0.5)
+	assert f.der == np.array([[2/0.5]])
+	assert f.jacobian == np.array([[1/0.5]])
 	# derivative not defined at x = 0
 	with pytest.warns(RuntimeWarning):
 		Y = AutoDiff(0, 2)
@@ -503,12 +504,12 @@ def test_log_results():
 		Y = AutoDiff(-0.5, 2)
 		f = ef.log(Y)
 		assert np.isnan(f.val)
-		assert f.der == -4
-		assert f.jacobian == -2
+		assert f.der == np.array([[2/-0.5]])
+		assert f.jacobian == np.array([[1/-0.5]])
 
 def test_log_constant_results():
 	a = ef.log(0.5)
-	assert a == -0.6931471805599453
+	assert a == np.log(0.5)
 	with pytest.warns(RuntimeWarning):
 		b = ef.log(0)
 		assert np.isneginf(b)
@@ -529,9 +530,9 @@ def test_log10_results():
 	# derivative defined at positive real numbers x > 0
 	X = AutoDiff(0.5, 2)
 	f = ef.log10(X)
-	assert f.val == -0.3010299956639812
-	assert f.der == 1.737177927613007
-	assert f.jacobian == 0.8685889638065035
+	assert f.val == np.log10(0.5)
+	assert f.der == np.array([[(1/((0.5)*np.log(10)))*2]])
+	assert f.jacobian == np.array([[(1/((0.5)*np.log(10)))*1]])
 	# neither value nor derivative defined at x = 0
 	with pytest.warns(RuntimeWarning):
 		Y = AutoDiff(0, 2)
@@ -544,12 +545,12 @@ def test_log10_results():
 		Y = AutoDiff(-0.5, 2)
 		f = ef.log10(Y)
 		assert np.isnan(f.val)
-		assert f.der == (2/(-0.5*np.log(10)))
-		assert f.jacobian == 1/(-0.5*np.log(10))
+		assert f.der == np.array([[(2/(-0.5*np.log(10)))]])
+		assert f.jacobian == np.array([[1/(-0.5*np.log(10))]])
 
 def test_log10_constant_results():
 	a = ef.log10(0.5)
-	assert a == -0.3010299956639812
+	assert a == np.log10(0.5)
 	with pytest.warns(RuntimeWarning):
 		b = ef.log10(0)
 		assert np.isinf(b)
@@ -569,9 +570,9 @@ def test_sqrt_ad_results():
 	# Positive reals
 	x = AutoDiff(0.5, 2.0)
 	f = ef.sqrt(x)
-	assert f.val == np.array([[0.7071067811865476]])
+	assert f.val == np.array([[np.sqrt(0.5)]])
 	assert f.der == np.array([[0.5 * 0.5 ** (-0.5) * 2.0]])
-	assert f.jacobian == np.array([[0.5 * 0.5 ** (-0.5)]])
+	assert f.jacobian == np.array([[0.5 * 0.5 ** (-0.5) * 1]])
 	# Value defined but derivative undefined when x == 0
 	with pytest.warns(RuntimeWarning):
 		y = AutoDiff(0, 2)
@@ -589,9 +590,9 @@ def test_sqrt_ad_results():
 
 def test_sqrt_constant_results():
 	a = ef.sqrt(5)
-	assert a == 2.23606797749979
+	assert a == np.sqrt(5)
 	b = ef.sqrt(0)
-	assert b == 0.0
+	assert b == np.sqrt(0)
 	# Value undefined when x < 0
 	with pytest.warns(RuntimeWarning):
 		c = ef.sqrt(-5)
